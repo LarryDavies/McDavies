2c5ae1423f8a433f9a90bb3a34a64d26e7b6de0c
Guru <larrydavies199@gmail.com>
2024-06-27 14:02:41 +0100

    made some changes

--- great.sh
+++ great.sh
@@ -1,2 +1,4 @@
 Tech is driving me crazy. It giving me joy and craziness at the same thing time. 
 Sometimes i do feel like giving up, but giving is gay.
+I can't give up. I will see this through to the end.
+Strong people don't give up, only weak people do.
